1dd2a928c0f255f3548958c869c36606f01b6633
Dave Does Demos <46533954+davedoesdemos@users.noreply.github.com>
2020-03-30 18:21:27 +0100

    added function app code

--- BlobArchiveFunction/BlobArchiveFunction/Function1.cs
+++ BlobArchiveFunction/BlobArchiveFunction/Function1.cs
@@ -1,27 +1,67 @@
-using System;
-using System.IO;
+using System; 
 using Microsoft.Azure.WebJobs;
-using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Azure.WebJobs.Extensions.EventGrid;
 using Microsoft.Extensions.Logging;
-using Microsoft.Azure.Storage;
 using Microsoft.Azure.Storage.Blob;
+using Microsoft.Azure.EventGrid.Models;
+using Newtonsoft.Json;
+using Microsoft.Extensions.Configuration;
+using System.Text.RegularExpressions;
 
 namespace BlobArchiveFunction
 {
+
     public static class Function1
     {
-        [FunctionName("Function1")]
-        public static void Run([BlobTrigger("incoming/{name}", Connection = "storageAccountConnection")]Stream myBlob, string name, ILogger log)
+        [FunctionName("ArchiveBlobs")]
+        public static void EventGridTest([EventGridTrigger]EventGridEvent eventGridEvent, ILogger log, ExecutionContext context)
         {
-            log.LogInformation($"C# Blob trigger function Processed blob\n Name:{name} \n Size: {myBlob.Length} Bytes");
-            // Azure.Storage.Blobs.Specialized.BlobBaseClient
-            CloudStorageAccount storageAccount = Common.CreateStorageAccountFromConnectionString();
-            var cloudBlobClient = storageAccount.CreateCloudBlobClient();
-            var container = cloudBlobClient.GetContainerReference("container");
-            var blob = container.GetBlockBlobReference("blob name");
-            blob.SetStandardBlobTier(StandardBlobTier.Cool);
-            blob.FetchAttributes();
-            var tier = blob.Properties.StandardBlobTier;
+            //get the storage account info from application settings
+            var config = new ConfigurationBuilder()
+                .SetBasePath(context.FunctionAppDirectory)
+                .AddJsonFile("local.settings.json", optional: true, reloadOnChange: true)
+                .AddEnvironmentVariables()
+                .Build();
+            var blobAccount = config["blobAccount"];
+
+            //get the data from the event trigger and deserialise the JSON
+            dynamic dataObject = JsonConvert.DeserializeObject(eventGridEvent.Data.ToString());
+
+            //get the whole url
+            string url = dataObject.url;
+
+            //find and extract the object path from the url
+            Regex regex = new Regex(@"blob.core.windows.net\/(.*)");
+            Match match = regex.Match(url);
+            if (match.Success)
+            {
+                string path = match.Groups[1].Value;
+                log.LogInformation("Path: " + path);
+                //find the blob details for the container and object
+                //NOTE: These can be filtered here should you need to restrict archival to one container
+
+                //container name
+                string container = path.Substring(0, path.IndexOf('/'));
+                log.LogInformation("Container: " + container);
+
+                //object path including hierarchy for ADLS Gen2
+                string blob = path.Substring(path.IndexOf('/'), (path.Length - path.IndexOf('/')));
+                log.LogInformation("Blob: " + blob);
+
+                //Connect to cloud storage and set the object to archive tier
+                var cloudBlobClient = new CloudBlobClient(new System.Uri(blobAccount));
+                var containerobj = cloudBlobClient.GetContainerReference(container);
+                var blobobj = containerobj.GetBlockBlobReference(blob);
+                try 
+                {
+                    blobobj.SetStandardBlobTier(StandardBlobTier.Archive);
+                    log.LogInformation("Archived " + blob);
+                }
+                catch (Exception e)
+                {
+                    log.LogError("Error: " + e.Message);
+                }
+            }
         }
     }
 }
